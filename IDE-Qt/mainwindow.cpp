cad3438ce6ee681ed3426857f819470b8ca8b517
王逍遥 <wangxiaoyao77@gmail.com>
2020-01-26 17:31:48 +0800

    文件-打开功能

--- IDE-Qt/mainwindow.cpp
+++ IDE-Qt/mainwindow.cpp
@@ -1,6 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <iostream>
+#include <QFileDialog>
+#include <QString>
+#include <QTextCodec>
+#include <QDebug>
+#define couts qDebug()
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -17,6 +23,41 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_actionopen_triggered()
 {
-    std::cout<< "nihao "<< std::endl;
+    QString fileName = QFileDialog::getOpenFileName();
+    couts <<"==="<< fileName <<"====";
 
+    /*
+    // Qstring 类型和 char* 类型。需要先转类型,或者使用QFile类
+    // fromUnicode 为将QString转为char *
+    QTextCodec *codec = QTextCodec::codecForName("utf-8");
+    char * file = codec->fromUnicode(fileName).data();
+    FILE *fp = fopen(file,"r");
+    if(!fp){
+        return;
+    }
+    couts << "文件打开成功";
+
+    char buf[1024];
+    QString txt;
+    while (!feof(fp)) {
+        memset(buf,0,1024);
+        fgets(buf,1024,fp);
+        txt += codec->toUnicode(buf);
+    }
+    // 我们需要将char *类型转为QString类型
+    ui->textEdit->setText(txt);
+    fclose(fp);
+    */
+    QFile file(fileName);
+    bool isok = file.open(QIODevice::ReadOnly);
+    if(!isok){
+        couts<<"文件打开失败";
+        return;
+    }
+    QByteArray arr;
+    while (!file.atEnd()) {
+        arr+= file.readLine();
+    }
+    ui->textEdit->setText(arr);
+    file.close();
 }
